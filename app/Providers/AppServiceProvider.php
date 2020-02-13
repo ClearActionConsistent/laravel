dbc1166f4b70dc1de31eb003f5916474a094f241
Trung Nguyen <trung.nguyenchi@brightstart.com>
2020-02-13 09:51:32 +0700

    [view composer] specify views need to share data with

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,6 +26,12 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
 		//case 1: this view will be generated on every time a page get created.
-        View::share('priceCodes', PriceCode::orderBy('id')->get());
+        //View::share('priceCodes', PriceCode::orderBy('id')->get());
+		
+		//case 2: specify views 
+		View::composer(['movie.create', 'movie.update'], function($view){
+			$view->with('priceCodes', PriceCode::orderBy('id')->get()
+			);
+		});
     }
 }
